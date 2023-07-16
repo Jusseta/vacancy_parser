c449ad7d55f8c338e89d75a27271d505a626d667
Victoria Kashpur <vikki0011@gmail.com>
2023-07-17 00:02:11 +1000

    edit docstrings and add if True in get_from_file

--- src/vacancies.py
+++ src/vacancies.py
@@ -5,7 +5,7 @@ import datetime
 
 
 class Vacancy:
-    '''Класс для инициализации вакансий'''
+    """Класс для инициализации вакансий"""
     def __init__(self, platform, title, url, salary_from, salary_to, date, employer):
         self.platform = platform
         self.title = title
@@ -49,89 +49,91 @@ class AbstractVacancyFile(ABC):
 
 
 class VacancyFile(AbstractVacancyFile):
-    '''Класс для добавления вакансий в json файл и работы с ним'''
+    """Класс для добавления вакансий в json файл и работы с ним"""
     def __init__(self):
         self.file_name = 'vacancies.json'
         self.vacancies = []
 
     def add_to_file(self, hh_vacancies: list, sj_vacancies: list):
-        '''
+        """
         Добавляет вакансии в json файл
         :param hh_vacancies: список вакансий с HH
         :param sj_vacancies: список вакансий с SJ
         :return: None
-        '''
+        """
         with open(f'hh_{self.file_name}', 'w', encoding='utf-8') as file:
             json.dump(hh_vacancies, file, ensure_ascii=False, indent=4)
         with open(f'sj_{self.file_name}', 'w', encoding='utf-8') as file:
             json.dump(sj_vacancies, file, ensure_ascii=False, indent=4)
 
     def get_from_file(self, currency: str, salary: int):
-        '''
+        """
         Читает json файл и обавляет в список vacancies подходящие словари
         :param currency: валюта
         :param salary: минимальная зарплата
         :return: None
-        '''
+        """
         with open(f'hh_{self.file_name}', 'r', encoding='utf-8') as file:
             hh_data = json.load(file)
 
             if currency == 'rub':
                 currency = 'rur'
 
-            for vac in hh_data:
-                if vac['salary'] and vac['salary']['from']:
-                    if vac['salary']['from'] >= salary:
-                        salary_from = vac['salary']['from']
-                        salary_to = vac['salary']['to'] if vac['salary']['to'] else 'Не указано'
+            if hh_data:
+                for vac in hh_data:
+                    if vac['salary'] and vac['salary']['from']:
+                        if vac['salary']['from'] >= salary:
+                            salary_from = vac['salary']['from']
+                            salary_to = vac['salary']['to'] if vac['salary']['to'] else 'Не указано'
 
-                        if vac['salary']['currency']:
-                            if vac['salary']['currency'] == currency.upper():
-                                raw_date = vac['published_at']
-                                date = datetime.datetime.fromisoformat(raw_date).strftime('%d.%m.%Y')
-                                data = {
-                                    'platform': 'HeadHunter',
-                                    'title': vac['name'],
-                                    'url': 'https://hh.ru/vacancy/' + vac['id'],
-                                    'salary_from': salary_from,
-                                    'salary_to': salary_to,
-                                    'date': date,
-                                    'employer': vac['employer']['name']
-                                        }
+                            if vac['salary']['currency']:
+                                if vac['salary']['currency'] == currency.upper():
+                                    raw_date = vac['published_at']
+                                    date = datetime.datetime.fromisoformat(raw_date).strftime('%d.%m.%Y')
+                                    data = {
+                                        'platform': 'HeadHunter',
+                                        'title': vac['name'],
+                                        'url': 'https://hh.ru/vacancy/' + vac['id'],
+                                        'salary_from': salary_from,
+                                        'salary_to': salary_to,
+                                        'date': date,
+                                        'employer': vac['employer']['name']
+                                            }
 
-                                self.vacancies.append(data)
+                                    self.vacancies.append(data)
 
         with open(f'sj_{self.file_name}', 'r', encoding='utf-8') as file:
             sj_data = json.load(file)
 
-            for vac in sj_data:
-                if vac['payment_from'] >= salary:
-                    salary_from = vac['payment_from']
-                    salary_to = vac['payment_to'] if vac['payment_to'] != 0 else 'Не указано'
+            if sj_data:
+                for vac in sj_data:
+                    if vac['payment_from'] >= salary:
+                        salary_from = vac['payment_from']
+                        salary_to = vac['payment_to'] if vac['payment_to'] != 0 else 'Не указано'
 
-                    if vac['currency'] == currency.lower():
-                        date_raw = vac['date_published']
-                        date_published = datetime.datetime.fromtimestamp(date_raw)
-                        date = date_published.strftime('%d.%m.%Y')
+                        if vac['currency'] == currency.lower():
+                            date_raw = vac['date_published']
+                            date_published = datetime.datetime.fromtimestamp(date_raw)
+                            date = date_published.strftime('%d.%m.%Y')
 
-                        data = {
-                            'platform': 'SuperJob',
-                            'title': vac['profession'],
-                            'url': vac['link'],
-                            'salary_from': salary_from,
-                            'salary_to': salary_to,
-                            'date': date,
-                            'employer': vac['firm_name']
-                                }
+                            data = {
+                                'platform': 'SuperJob',
+                                'title': vac['profession'],
+                                'url': vac['link'],
+                                'salary_from': salary_from,
+                                'salary_to': salary_to,
+                                'date': date,
+                                'employer': vac['firm_name']
+                                    }
 
-                        self.vacancies.append(data)
+                            self.vacancies.append(data)
 
     def delete_from_file(self, keyword):
-        '''
+        """
         Удаляет из json файла неподходящие по занятости вакансии
         :param keyword: занятость
         :return: None
-        '''
+        """
         with open(f'sj_{self.file_name}', 'r', encoding='utf-8') as file:
             data = json.load(file)
             for i in reversed(range(len(data))):
@@ -151,11 +153,11 @@ class VacancyFile(AbstractVacancyFile):
                 json.dump(data, f, ensure_ascii=False, indent=4)
 
     def sort_by_salary(self):
-        '''Сортирует список vacancies по зарплате (от большего к меньшему)'''
+        """Сортирует список vacancies по зарплате (от большего к меньшему)"""
         vacancies = self.vacancies.sort(key=lambda k: k['salary_from'], reverse=True)
         return vacancies
 
     def sort_by_date(self):
-        '''Сортирует список vacancies по дате (от новых к старым)'''
+        """Сортирует список vacancies по дате (от новых к старым)"""
         vacancies = self.vacancies.sort(key=lambda k: k['date'], reverse=True)
         return vacancies
